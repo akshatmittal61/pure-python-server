c30d86073340f046a413720c665034499a3640ed
Akshat Mittal <akshatmittal2506@gmail.com>
2024-03-09 15:20:58 +0530

    creating watcher

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from json import dumps
+import subprocess
+import sys
+import os
+import time
+from multiprocessing import Process
 
-class MyRequestHandler(BaseHTTPRequestHandler):
-    """
-    GET requests handler
-    """
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
-        response_data = { 'message': 'success', 'data': 'Server health is good' }
-        response_json = dumps(response_data)
-        self.wfile.write(response_json.encode('utf-8'))
-        return self
+ignored_files
 
-def handler_factory(*args):
-    return MyRequestHandler(*args)
+def run_server():
+    subprocess.run([sys.executable, "main.py"])
 
-def run():
-    httpd = None
+def get_file_modified_times():
+    return {filename: os.path.getmtime(filename) for filename in os.listdir('.') if filename not in ['main.py', '.git', 'venv', '.idea']}
+
+def monitor_changes():
+    files = {}
     try:
-        server_address = ('127.0.0.1', 8000)
-        httpd = HTTPServer(server_address, handler_factory)
-        print('Server is listening')
-        httpd.serve_forever()
+        while True:
+            time.sleep(1)  # Check for changes every second
+            new_files = get_file_modified_times()
+            if new_files != files:
+                print(files, new_files)
+                print("Changes detected, restarting server...")
+                files = new_files
+                # Start the server in a separate process
+                p = Process(target=run_server)
+                p.start()
+                p.join()  # Wait for the server process to complete
     except KeyboardInterrupt:
-        print('Terminating connections')
-        httpd.socket.close()
-    except Exception as e:
-        print('Error occurred:', e)
-        httpd.socket.close()
+        print('Terminating watcher')
+        exit(0)
 
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    monitor_changes()
